ea62330ac445d052e2c4c9a3963cb9a94b952c6f
Alexandros Chatzikyriakos <chatzialex@yahoo.gr>
2024-02-18 12:03:51 +0000

    refactor move_shlelf_to_ship.py: don't spin unless necessary

--- nav2_apps/scripts/move_shelf_to_ship.py
+++ nav2_apps/scripts/move_shelf_to_ship.py
@@ -11,15 +11,9 @@ from std_msgs.msg import Empty
 from std_srvs.srv import Trigger
 
 
-class MoveShelfToShip(Node):
-    goals = {
-        'initial_position': {'position': {'x': 0.0, 'y': 0.0}, 'orientation': {'z': 0.0, 'w': 1.0}},
-        'loading position': {'position': {'x': 5.5, 'y': 0.0}, 'orientation': {'z': -0.707, 'w': 0.707}},
-        'shipping_position': {'position': {'x': 2.5, 'y': 1.3}, 'orientation': {'z': 0.707, 'w': 0.707}}
-    }
-
-    def __init__(self):
-        super().__init__('move_shelf_to_ship')
+class MoveShelfToShipNode(Node):
+    def __init__(self, node_name):
+        super().__init__(node_name)
 
         self.publisher_ = self.create_publisher(
             Empty,
@@ -32,10 +26,20 @@ class MoveShelfToShip(Node):
 
         self.client_ = self.create_client(Trigger, 'approach_shelf')
         while not self.client_.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Service not available, waiting again...')
+            print('Service not available, waiting again...')
+
+class MoveShelfToShip():
+    goals = {
+        'initial_position': {'position': {'x': 0.0, 'y': 0.0}, 'orientation': {'z': 0.0, 'w': 1.0}},
+        'loading position': {'position': {'x': 5.5, 'y': 0.0}, 'orientation': {'z': -0.707, 'w': 0.707}},
+        'shipping_position': {'position': {'x': 2.5, 'y': 1.3}, 'orientation': {'z': 0.707, 'w': 0.707}}
+    }
+
+    def __init__(self):
+        self.node_ = MoveShelfToShipNode('move_shelf_to_ship_node')
 
         self.navigator_ = BasicNavigator()
-        self.route = {}
+        self.route_ = {}
         waypoint = PoseStamped()
         waypoint.header.frame_id = 'map'
         for name, pt in self.goals.items():
@@ -43,16 +47,12 @@ class MoveShelfToShip(Node):
             waypoint.pose.position.y = pt['position']['y']
             waypoint.pose.orientation.z = pt['orientation']['z']
             waypoint.pose.orientation.w = pt['orientation']['w']
-            self.route[name] = deepcopy(waypoint)
-        self.route['initial_position'].header.stamp = self.navigator_.get_clock().now().to_msg()
-        self.navigator_.setInitialPose(self.route['initial_position'])
+            self.route_[name] = deepcopy(waypoint)
+        self.route_['initial_position'].header.stamp = self.navigator_.get_clock().now().to_msg()
+        self.navigator_.setInitialPose(self.route_['initial_position'])
         self.navigator_.waitUntilNav2Active()
 
-        self.timer = self.create_timer(0.2, self.timerCb)
-
-    def timerCb(self):
-        self.timer.cancel()
-
+    def perform_motion(self):
         success = (self.goToPose('loading position')
                    and self.attachToShelf()
                    and self.goToPose('shipping_position')
@@ -60,44 +60,48 @@ class MoveShelfToShip(Node):
 
         self.goToPose("initial_position")
 
+
     def goToPose(self, pose_name):
-        self.route[pose_name].header.stamp = self.navigator_.get_clock().now().to_msg()
-        self.navigator_.goToPose(self.route[pose_name])
+        self.route_[pose_name].header.stamp = self.navigator_.get_clock().now().to_msg()
+        self.navigator_.goToPose(self.route_[pose_name])
 
         i = 0
         while not self.navigator_.isTaskComplete():
             i = i + 1
             feedback = self.navigator_.getFeedback()
             if feedback and i % 20 == 0:
-                self.get_logger().info(f'Estimated time of arrival at {pose_name}: '
+                print(f'Estimated time of arrival at {pose_name}: '
                     f'{Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9} '
                     'seconds')
 
         result = self.navigator_.getResult()
         if result == TaskResult.SUCCEEDED:
-            self.get_logger().info(f'Arrived at {pose_name}.')
+            print(f'Arrived at {pose_name}.')
             return True
         elif result == TaskResult.CANCELED:
-            self.get_logger().info(f'Route to {pose_name} was cancelled.')
+            print(f'Route to {pose_name} was cancelled.')
             return False
         elif result == TaskResult.FAILED:
-            self.get_logger().warn(f'Failed to arrive to {pose_name}.')
+            print(f'Failed to arrive to {pose_name}.')
             return False
 
     def attachToShelf(self):
-        self.get_logger().info('Attempting to attach shelf.')
-        future = self.client_.call_async()
-        rclpy.spin_until_future_complete(self, future)
+        print('Attempting to attach shelf.')
+        request = Trigger.Request()
+        future = self.node_.client_.call_async(request)
+        rclpy.spin_until_future_complete(self.node_, future)
         if future.result().success:
-            self.get_logger().info('Successfully attached to shelf.')
+            print('Successfully attached to shelf.')
         else:
-            self._logger().warn('Failed to attach to shelf.')
+            print('Failed to attach to shelf.')
+        return future.result().success
 
 def main():
     rclpy.init()
-    node = MoveShelfToShip()
-    rclpy.spin(node)
-    node.destroy_node()
+
+    move_shelf_to_ship = MoveShelfToShip()
+    move_shelf_to_ship.perform_motion()
+
     rclpy.shutdown()
 
 if __name__ == '__main__':
